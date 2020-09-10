f4fd62711c700dbc76c837b144540d82af7c2567
Major Hayden <major@redhat.com>
2020-09-10 12:07:04 -0500

    👂 Set a custom listen address if provided
    When running in OpenShift, installer needs to listen on a different
    address (and possibly different port, too). Allow a user to specify a
    custom listen address via environment variable.
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- cmd/osbuild-installer/main.go
+++ cmd/osbuild-installer/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"os"
+
 	"github.com/osbuild/osbuild-installer/internal/server"
 )
 
 func main() {
-	server.Run("localhost:8086")
+	// Look for a custom listen address in environment variables.
+	listenAddress, ok := os.LookupEnv("LISTEN_ADDRESS")
+
+	// Listen on localhost otherwise.
+	if !ok {
+		listenAddress = "localhost:8086"
+	}
+
+	// Run the server and listen for requests.
+	server.Run(listenAddress)
 }
