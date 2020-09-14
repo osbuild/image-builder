52dbd3424261cb0e45b6e30630fd202a4b487ad4
sanne raymaekers <sanne.raymaekers@gmail.com>
2020-09-14 14:56:55 +0200

    schutzbot: Add mockbuild

---
+++ schutzbot/mockbuild.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -euo pipefail
+
+# Colorful output.
+function greenprint {
+  echo -e "\033[1;32m${1}\033[0m"
+}
+
+# Get OS and architecture details.
+source /etc/os-release
+ARCH=$(uname -m)
+
+# Mock is only available in EPEL for RHEL.
+if [[ $ID == rhel ]] && ! rpm -q epel-release; then
+    greenprint "📦 Setting up EPEL repository"
+    curl -Ls --retry 5 --output /tmp/epel.rpm \
+        https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+    sudo rpm -Uvh /tmp/epel.rpm
+fi
+
+# Register RHEL if we are provided with a registration script.
+if [[ -n "${RHN_REGISTRATION_SCRIPT:-}" ]] && ! sudo subscription-manager status; then
+    greenprint "🪙 Registering RHEL instance"
+    sudo chmod +x $RHN_REGISTRATION_SCRIPT
+    sudo $RHN_REGISTRATION_SCRIPT
+fi
+
+# Install requirements for building RPMs in mock.
+greenprint "📦 Installing mock requirements"
+sudo dnf -y install createrepo_c make mock python3-pip rpm-build
+
+# Jenkins sets a workspace variable as the root of its working directory.
+WORKSPACE=${WORKSPACE:-$(pwd)}
+
+# Mock configuration file to use for building RPMs.
+MOCK_CONFIG="${ID}-${VERSION_ID%.*}-$(uname -m)"
+
+# Jenkins takes the proposed PR and merges it onto master. Although this
+# creates a new SHA (which is slightly confusing), it ensures that the code
+# merges properly against master and it tests the code against the latest
+# commit in master, which is certainly good.
+POST_MERGE_SHA=$(git rev-parse --short HEAD)
+
+# Directory to hold the RPMs temporarily before we upload them.
+REPO_DIR=repo/${JOB_NAME}/${POST_MERGE_SHA}/${ID}${VERSION_ID//./}_${ARCH}
+
+# Build source RPMs.
+greenprint "🔧 Building source RPMs."
+make srpm
+
+# Compile RPMs in a mock chroot
+greenprint "🎁 Building RPMs with mock"
+sudo mock -v -r $MOCK_CONFIG --resultdir $REPO_DIR --with=tests \
+    rpmbuild/SRPMS/*.src.rpm
