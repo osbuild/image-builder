afdc80fdc530384c0733fc0c619b0484e7835cc5
Major Hayden <major@redhat.com>
2020-11-03 10:53:46 -0600

    🤷 openshift: Fix CW var in bash script
    Signed-off-by: Major Hayden <major@redhat.com>

--- distribution/openshift-startup.sh
+++ distribution/openshift-startup.sh
@@ -5,7 +5,7 @@ if [[ -z "${KUBERNETES_PORT:-}" ]]; then
     echo "Starting image-builder inside container..."
 else
     echo "Starting image-builder inside OpenShift..."
-    echo "Cloudwatch logs: ${CW_LOG_GROUP_NAME} in ${CW_AWS_REGION}"
+    echo "Cloudwatch: ${CW_LOG_GROUP} in ${CW_AWS_REGION}"
     echo "Database: ${PGDATABASE} on ${PGHOST}:${PGPORT}"
 fi
 
