02cf9283c98cbd4acb34b2b8fd23751f16e18cc4
Aleksandar Todorov <atodorov@redhat.com>
2020-12-03 14:57:54 +0200

    tests: Add tests for cloudapi package

---
+++ internal/cloudapi/client_test.go
@@ -0,0 +1,35 @@
+package cloudapi
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNewOsbuildClientWithoutCerts(t *testing.T) {
+	client := NewOsbuildClient("localhost:8086", nil, nil, nil)
+
+	require.Equal(t, "", client.ca)
+	require.Equal(t, "", client.cert)
+	require.Equal(t, "", client.key)
+}
+
+func TestNewOsbuildClientWithCerts(t *testing.T) {
+	myCert := "test-cert"
+	myKey := "test-key"
+	myCA := "test-ca"
+	client := NewOsbuildClient("localhost:8086", &myCert, &myKey, &myCA)
+
+	require.NotEqual(t, "", client.ca)
+	require.NotEqual(t, "", client.cert)
+	require.NotEqual(t, "", client.key)
+}
+
+func TestOsbuildClientGetNoError(t *testing.T) {
+	// we're not using HTTPS URL on purpose
+	client := NewOsbuildClient("localhost:8086/", nil, nil, nil)
+	new_client, err := client.Get()
+
+	require.NoError(t, err)
+	require.NotNil(t, new_client)
+	require.Equal(t, "localhost:8086/", new_client.Server)
+}
