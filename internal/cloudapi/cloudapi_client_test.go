d5629c235740f98497fe284f9d81ebddb3d644f6
Aleksandar Todorov <atodorov@redhat.com>
2020-12-08 13:51:19 +0200

    tests: Add tests for cloudapi_client.go

---
+++ internal/cloudapi/cloudapi_client_test.go
@@ -0,0 +1,29 @@
+package cloudapi
+
+import (
+	"errors"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+)
+
+func TestNewClientWithoutOptions(t *testing.T) {
+	// note: URL is without trailling slash on purpose
+	result, err := NewClient("example.com")
+
+	require.NoError(t, err)
+	require.NotNil(t, result.Client)
+	require.Equal(t, http.DefaultClient, result.Client)
+	// verify server URL always has a trailing slash
+	require.Equal(t, "example.com/", result.Server)
+}
+
+func TestNewClientErrorHandlingFromClientOptions(t *testing.T) {
+	configureClientWithErrors := func(client *Client) error {
+		return errors.New("Expected during testing")
+	}
+	result, err := NewClient("example.com/", configureClientWithErrors)
+
+	require.Nil(t, result)
+	require.EqualError(t, err, "Expected during testing")
+}
