a9e41e4e2cf0824627792009565e9a4d855894b1
Aleksandar Todorov <atodorov@redhat.com>
2021-01-14 12:22:17 +0200

    tests: Don't use auto-generated client

--- cmd/image-builder-tests/main_test.go
+++ cmd/image-builder-tests/main_test.go
@@ -4,7 +4,7 @@ package main
 
 import (
 	"bytes"
-	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,31 +12,19 @@ import (
 	"testing"
 	"time"
 
+	"github.com/osbuild/image-builder/internal/cloudapi"
 	"github.com/osbuild/image-builder/internal/server"
+	"github.com/osbuild/image-builder/internal/tutils"
 
 	"github.com/stretchr/testify/require"
 )
 
-func AddDummyIdentityHeader(ctx context.Context, request *http.Request) error {
-	request.Header.Add("x-rh-identity", "eyJlbnRpdGxlbWVudHMiOnsiaW5zaWdodHMiOnsiaXNfZW50aXRsZWQiOnRydWV9LCJzbWFydF9tYW5hZ2VtZW50Ijp7ImlzX2VudGl0bGVkIjp0cnVlfSwib3BlbnNoaWZ0Ijp7ImlzX2VudGl0bGVkIjp0cnVlfSwiaHlicmlkIjp7ImlzX2VudGl0bGVkIjp0cnVlfSwibWlncmF0aW9ucyI6eyJpc19lbnRpdGxlZCI6dHJ1ZX0sImFuc2libGUiOnsiaXNfZW50aXRsZWQiOnRydWV9fSwiaWRlbnRpdHkiOnsiYWNjb3VudF9udW1iZXIiOiIwMDAwMDAiLCJ0eXBlIjoiVXNlciIsInVzZXIiOnsidXNlcm5hbWUiOiJ1c2VyIiwiZW1haWwiOiJ1c2VyQHVzZXIudXNlciIsImZpcnN0X25hbWUiOiJ1c2VyIiwibGFzdF9uYW1lIjoidXNlciIsImlzX2FjdGl2ZSI6dHJ1ZSwiaXNfb3JnX2FkbWluIjp0cnVlLCJpc19pbnRlcm5hbCI6dHJ1ZSwibG9jYWxlIjoiZW4tVVMifSwiaW50ZXJuYWwiOnsib3JnX2lkIjoiMDAwMDAwIn19fQ==")
-	return nil
-}
-
-func ConfigureClient(client *server.Client) error {
-	client.RequestEditor = AddDummyIdentityHeader
-	return nil
-}
-
 func RunTestWithClient(t *testing.T, ib string)  {
-	client, err := server.NewClientWithResponses(ib, ConfigureClient)
-	require.NoError(t, err)
-
 	// wait until server is reachable
-	ctx := context.Background()
 	tries := 0
 	for tries  < 5 {
-		resp, err := client.GetVersionWithResponse(ctx)
-		if err == nil && resp.StatusCode() == http.StatusOK {
+		resp, err := tutils.GetResponseError(ib + "/version")
+		if err == nil && resp.StatusCode == http.StatusOK {
 			break
 		} else if tries == 4 {
 			require.NoError(t, err)
@@ -45,26 +33,35 @@ func RunTestWithClient(t *testing.T, ib string)  {
 		tries += 1
 	}
 
-	distroResp, err := client.GetDistributionsWithResponse(ctx)
-	require.NoError(t, err)
-	require.Equalf(t, http.StatusOK, distroResp.StatusCode(), "Error: got non-200 status. Full response: %s", distroResp.Body)
-	require.NotNil(t, distroResp.JSON200)
-	require.NotEmpty(t, distroResp.JSON200)
+	response, body := tutils.GetResponseBody(t, ib + "/distributions", true)
+	require.Equalf(t, http.StatusOK, response.StatusCode, "Error: got non-200 status. Full response: %s", body)
 
-	distro := (*distroResp.JSON200)[0].Name
-	archResp, err := client.GetArchitecturesWithResponse(ctx, distro)
+	require.NotNil(t, body)
+	require.NotEmpty(t, body)
+
+	var distributions server.Distributions
+	err := json.Unmarshal([]byte(body), &distributions)
 	require.NoError(t, err)
-	require.Equalf(t, http.StatusOK, archResp.StatusCode(), "Error: got non-200 status. Full response: %s", archResp.Body)
-	require.NotEmpty(t, archResp.JSON200)
+
+	distro := distributions[0].Name
+	response, body = tutils.GetResponseBody(t, ib + "/architectures/" + distro, true)
+	require.Equalf(t, http.StatusOK, response.StatusCode, "Error: got non-200 status. Full response: %s", body)
+
+	require.NotNil(t, body)
+	require.NotEmpty(t, body)
+
+	var architectures server.Architectures
+	err = json.Unmarshal([]byte(body), &architectures)
+	require.NoError(t, err)
 
 
 	// Build a composerequest
-	composeRequest := server.ComposeImageJSONRequestBody{
+	composeRequest := server.ComposeRequest{
 		Distribution: distro,
 		ImageRequests: []server.ImageRequest{
 			server.ImageRequest{
-				Architecture: (*archResp.JSON200)[0].Arch,
-				ImageType: (*archResp.JSON200)[0].ImageTypes[0],
+				Architecture: architectures[0].Arch,
+				ImageType: architectures[0].ImageTypes[0],
 				UploadRequests: []server.UploadRequest{
 					{
 						Type: "aws",
@@ -77,23 +74,33 @@ func RunTestWithClient(t *testing.T, ib string)  {
 		},
 	}
 
-	composeResp, err := client.ComposeImageWithResponse(ctx, composeRequest)
-	require.NoError(t, err)
-	require.Equal(t, http.StatusCreated, composeResp.StatusCode(), "Error: got non-201 status. Full response: %s", composeResp.Body)
-	require.NotNil(t, composeResp.JSON201)
-	id := composeResp.JSON201.Id
+	response, body = tutils.PostResponseBody(t, ib + "/compose", composeRequest)
+	require.Equal(t, http.StatusCreated, response.StatusCode, "Error: got non-201 status. Full response: %s", body)
 
-	statusResp, err := client.GetComposeStatusWithResponse(ctx, id)
+	require.NotNil(t, body)
+	require.NotEmpty(t, body)
+
+	var composeResp server.ComposeResponse
+	err = json.Unmarshal([]byte(body), &composeResp)
 	require.NoError(t, err)
-	require.Equal(t, http.StatusCreated, composeResp.StatusCode(), "Error: got non-201 status. Full response: %s", composeResp.Body)
-	require.NotEmpty(t, statusResp.JSON200)
-	require.Contains(t, []string{"pending", "running"}, statusResp.JSON200.Status)
+
+	id := composeResp.Id
+
+	response, body = tutils.GetResponseBody(t, ib + "/composes/" + id, true)
+	require.Equal(t, http.StatusOK, response.StatusCode, "Error: got non-200 status. Full response: %s", body)
+
+	require.NotNil(t, body)
+	require.NotEmpty(t, body)
+
+	var composeStatus cloudapi.ComposeStatus
+	err = json.Unmarshal([]byte(body), &composeStatus)
+	require.NoError(t, err)
+
+	require.Contains(t, []string{"pending", "running"}, composeStatus.Status)
 
 	// Check if we get 404 without the identity header
-	client, err = server.NewClientWithResponses(ib)
-	versionResp, err := client.GetVersionWithResponse(ctx)
-	require.NoError(t, err)
-	require.Equalf(t, http.StatusNotFound, versionResp.StatusCode(), "Error: got non-404 status. Full response: %s", versionResp.Body)
+	response, body = tutils.GetResponseBody(t, ib + "/version", false)
+	require.Equalf(t, http.StatusNotFound, response.StatusCode, "Error: got non-404 status. Full response: %s", body)
 }
 
 func TestImageBuilder(t *testing.T) {
---
+++ internal/tutils/tutils.go
@@ -0,0 +1,65 @@
+package tutils
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func GetResponseError(url string) (*http.Response, error) {
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Add("x-rh-identity", "eyJlbnRpdGxlbWVudHMiOnsiaW5zaWdodHMiOnsiaXNfZW50aXRsZWQiOnRydWV9LCJzbWFydF9tYW5hZ2VtZW50Ijp7ImlzX2VudGl0bGVkIjp0cnVlfSwib3BlbnNoaWZ0Ijp7ImlzX2VudGl0bGVkIjp0cnVlfSwiaHlicmlkIjp7ImlzX2VudGl0bGVkIjp0cnVlfSwibWlncmF0aW9ucyI6eyJpc19lbnRpdGxlZCI6dHJ1ZX0sImFuc2libGUiOnsiaXNfZW50aXRsZWQiOnRydWV9fSwiaWRlbnRpdHkiOnsiYWNjb3VudF9udW1iZXIiOiIwMDAwMDAiLCJ0eXBlIjoiVXNlciIsInVzZXIiOnsidXNlcm5hbWUiOiJ1c2VyIiwiZW1haWwiOiJ1c2VyQHVzZXIudXNlciIsImZpcnN0X25hbWUiOiJ1c2VyIiwibGFzdF9uYW1lIjoidXNlciIsImlzX2FjdGl2ZSI6dHJ1ZSwiaXNfb3JnX2FkbWluIjp0cnVlLCJpc19pbnRlcm5hbCI6dHJ1ZSwibG9jYWxlIjoiZW4tVVMifSwiaW50ZXJuYWwiOnsib3JnX2lkIjoiMDAwMDAwIn19fQ==")
+
+	return client.Do(request)
+}
+
+func GetResponseBody(t *testing.T, url string, auth bool) (*http.Response, string) {
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", url, nil)
+	require.NoError(t, err)
+	if auth {
+		request.Header.Add("x-rh-identity", "eyJlbnRpdGxlbWVudHMiOnsiaW5zaWdodHMiOnsiaXNfZW50aXRsZWQiOnRydWV9LCJzbWFydF9tYW5hZ2VtZW50Ijp7ImlzX2VudGl0bGVkIjp0cnVlfSwib3BlbnNoaWZ0Ijp7ImlzX2VudGl0bGVkIjp0cnVlfSwiaHlicmlkIjp7ImlzX2VudGl0bGVkIjp0cnVlfSwibWlncmF0aW9ucyI6eyJpc19lbnRpdGxlZCI6dHJ1ZX0sImFuc2libGUiOnsiaXNfZW50aXRsZWQiOnRydWV9fSwiaWRlbnRpdHkiOnsiYWNjb3VudF9udW1iZXIiOiIwMDAwMDAiLCJ0eXBlIjoiVXNlciIsInVzZXIiOnsidXNlcm5hbWUiOiJ1c2VyIiwiZW1haWwiOiJ1c2VyQHVzZXIudXNlciIsImZpcnN0X25hbWUiOiJ1c2VyIiwibGFzdF9uYW1lIjoidXNlciIsImlzX2FjdGl2ZSI6dHJ1ZSwiaXNfb3JnX2FkbWluIjp0cnVlLCJpc19pbnRlcm5hbCI6dHJ1ZSwibG9jYWxlIjoiZW4tVVMifSwiaW50ZXJuYWwiOnsib3JnX2lkIjoiMDAwMDAwIn19fQ==")
+	}
+
+	response, err := client.Do(request)
+	require.NoError(t, err)
+
+	body, err := ioutil.ReadAll(response.Body)
+	require.NoError(t, err)
+
+	err = response.Body.Close()
+	require.NoError(t, err)
+
+	return response, string(body)
+}
+
+func PostResponseBody(t *testing.T, url string, compose interface{}) (*http.Response, string) {
+	buf, err := json.Marshal(compose)
+	require.NoError(t, err)
+
+	client := &http.Client{}
+	request, err := http.NewRequest("POST", url, bytes.NewReader(buf))
+	require.NoError(t, err)
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("x-rh-identity", "eyJlbnRpdGxlbWVudHMiOnsiaW5zaWdodHMiOnsiaXNfZW50aXRsZWQiOnRydWV9LCJzbWFydF9tYW5hZ2VtZW50Ijp7ImlzX2VudGl0bGVkIjp0cnVlfSwib3BlbnNoaWZ0Ijp7ImlzX2VudGl0bGVkIjp0cnVlfSwiaHlicmlkIjp7ImlzX2VudGl0bGVkIjp0cnVlfSwibWlncmF0aW9ucyI6eyJpc19lbnRpdGxlZCI6dHJ1ZX0sImFuc2libGUiOnsiaXNfZW50aXRsZWQiOnRydWV9fSwiaWRlbnRpdHkiOnsiYWNjb3VudF9udW1iZXIiOiIwMDAwMDAiLCJ0eXBlIjoiVXNlciIsInVzZXIiOnsidXNlcm5hbWUiOiJ1c2VyIiwiZW1haWwiOiJ1c2VyQHVzZXIudXNlciIsImZpcnN0X25hbWUiOiJ1c2VyIiwibGFzdF9uYW1lIjoidXNlciIsImlzX2FjdGl2ZSI6dHJ1ZSwiaXNfb3JnX2FkbWluIjp0cnVlLCJpc19pbnRlcm5hbCI6dHJ1ZSwibG9jYWxlIjoiZW4tVVMifSwiaW50ZXJuYWwiOnsib3JnX2lkIjoiMDAwMDAwIn19fQ==")
+
+	response, err := client.Do(request)
+	require.NoError(t, err)
+
+	body, err := ioutil.ReadAll(response.Body)
+	require.NoError(t, err)
+
+	err = response.Body.Close()
+	require.NoError(t, err)
+
+	return response, string(body)
+}
