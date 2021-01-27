2ab118f8c78c7b6ea331284877465419dec5e40d
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2021-01-27 20:06:18 +0100

    distribution: Remove db variables echo from startup script
    Let's start doing this when the database PR is in.

--- distribution/openshift-startup.sh
+++ distribution/openshift-startup.sh
@@ -6,7 +6,6 @@ if [[ -z "${KUBERNETES_PORT:-}" ]]; then
 else
     echo "Starting image-builder inside OpenShift..."
     echo "Cloudwatch: ${CW_LOG_GROUP} in ${CW_AWS_REGION}"
-    echo "Database: ${PGDATABASE} on ${PGHOST}:${PGPORT}"
     echo "Upload target: ${OSBUILD_AWS_S3_BUCKET} in ${OSBUILD_AWS_REGION}"
     echo "Composer URL: ${OSBUILD_URL}"
 fi
