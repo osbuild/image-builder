a96417896637b45c48ee328cd6040863082dc5c7
Ondřej Budai <ondrej@budai.cz>
2021-02-17 15:00:20 +0100

    distributions: add centos stream 8 support
    We want to build moar distros!

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -72,7 +72,7 @@ func TestWithoutOsbuildComposerBackend(t *testing.T) {
 		require.NoError(t, err)
 
 		for _, distro := range result {
-			require.Contains(t, []string{"fedora-32", "rhel-8"}, distro.Name)
+			require.Contains(t, []string{"fedora-32", "rhel-8", "centos-8"}, distro.Name)
 		}
 	})
 
