87e953f407254c7e85c709850eb8caba57e9e789
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2021-05-17 13:00:57 +0200

    pr_check: Set up pr_check using bonfire/cicd scripts

--- pr_check.sh
+++ pr_check.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-# AppSRE runs this script to check pull requests. We will short circuit this
-# for now until we determine which tests we want to run.
-set -euxo pipefail
 
-exit 0
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+export APP_NAME="image-builder"  # name of app-sre "application" folder this component lives in
+export COMPONENT_NAME="image-builder"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+export IMAGE="quay.io/cloudservices/image-builder"
+
+# IQE_PLUGINS="image-builder"
+# IQE_MARKER_EXPRESSION="smoke"
+# IQE_FILTER_EXPRESSION=""
+
+echo "LABEL quay.expires-after=3d" >> ./Dockerfile # tag expire in 3 days
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s "$CICD_URL"/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+source "$CICD_ROOT"/build.sh
+source "$CICD_ROOT"/deploy_ephemeral_env.sh
+# source "$CICD_ROOT"/smoke_test.sh
