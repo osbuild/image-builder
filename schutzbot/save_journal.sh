18e26734d2213776b8578b942e3e05e0eaf8f9b0
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2021-12-16 13:21:31 +0100

    test: Save container log as artifact

--- schutzbot/save_journal.sh
+++ schutzbot/save_journal.sh
@@ -7,3 +7,7 @@ sudo journalctl --boot | tee journal-log >/dev/null
 # purposes, encrypt journal-log with a symmetric passphrase.
 gpg --batch --yes --passphrase "$GPG_SYMMETRIC_PASSPHRASE" -o journal-log.gpg --symmetric journal-log
 rm journal-log
+
+sudo podman logs image-builder | tee container-log >/dev/null
+gpg --batch --yes --passphrase "$GPG_SYMMETRIC_PASSPHRASE" -o container-log.gpg --symmetric container-log
+rm container-log
