6bfd7a9175ddc0b6c0427c1767cbd8292f900ff3
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2021-12-16 17:38:57 +0100

    schutzbot: Don't disable selinux during container build

--- schutzbot/build.sh
+++ schutzbot/build.sh
@@ -11,9 +11,7 @@ QUAY_REPO_URL="quay.io/osbuild/image-builder-test"
 QUAY_REPO_TAG="${CI_PIPELINE_ID}"
 
 sudo dnf install -y podman
-sudo setenforce 0 # todo
 greenprint "🎁 Building container"
 sudo podman build --label="quay.expires-after=1w" --security-opt "label=disable" -t image-builder-"${QUAY_REPO_TAG}" -f distribution/Dockerfile-ubi .
-sudo setenforce 1
 greenprint "🚀 Pushing container to test registry"
 sudo podman push --creds "${V2_QUAY_USERNAME}":"${V2_QUAY_PASSWORD}" image-builder-"${CI_PIPELINE_ID}" "${QUAY_REPO_URL}":"${QUAY_REPO_TAG}"
