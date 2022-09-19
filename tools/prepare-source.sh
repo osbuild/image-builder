c3aace4a0c3af6e356210fb41050585ebeaefc4c
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2022-09-19 11:04:00 +0200

    tools/prepare-source.sh: Run go mod generate before fmt and mod
    `go generate` somehow touches go.sum, which causes issues for dependabot
    PRs (osbuild/image-builder#418 for example). Run mod tidy command last.

--- tools/prepare-source.sh
+++ tools/prepare-source.sh
@@ -14,13 +14,12 @@ GO_BINARY=$(go env GOPATH)/bin/go$GO_VERSION
 go install golang.org/dl/go$GO_VERSION@latest
 $GO_BINARY download
 
-
-# ensure that go.mod and go.sum are up to date, ...
-$GO_BINARY mod tidy
-$GO_BINARY mod vendor
+# Ensure that all code has been regenerated from its sources
+$GO_BINARY generate ./...
 
 # ... the code is formatted correctly, ...
 $GO_BINARY fmt ./...
 
-# ... and all code has been regenerated from its sources.
-$GO_BINARY generate ./...
+# ... and that go.mod and go.sum are up to date.
+$GO_BINARY mod tidy
+$GO_BINARY mod vendor
