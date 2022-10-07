b6f586eb4bdcde6aa876b5b283e4a7dbab0b3131
Jakub Rusz <jrusz@redhat.com>
2022-10-07 15:40:15 +0200

    Enable testing in pr_check.sh
    We can now run IQE plugin tests on PRs in ephemeral environment. This
    script is filled with defaults as that serves our needs just fine. It
    builds the container image with the changes in the PR, provisions an
    ephemeral namespace, deployes image-builder, runs the IQE
    image-builder-backend test suite and reports back on the PR.

--- pr_check.sh
+++ pr_check.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
-# AppSRE runs this script to check pull requests. We will short circuit this
-# for now until we determine which tests we want to run.
-set -euxo pipefail
 
-exit 0
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+export APP_NAME="image-builder-crc"  # name of app-sre "application" folder this component lives in
+export COMPONENT_NAME="image-builder"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+export IMAGE="quay.io/cloudservices/image-builder"  # image location on quay
+
+export IQE_PLUGINS="image-builder-backend"  # name of the IQE plugin for this app.
+export IQE_CJI_TIMEOUT="30m"  # This is the time to wait for smoke test to complete or fail
+export DOCKERFILE="distribution/Dockerfile-ubi"
+
+# Install bonfire repo/initialize
+# https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd/bootstrap.sh
+# This script automates the install / config of bonfire
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s "$CICD_URL"/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+# The contents of build.sh can be found at:
+# https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd/build.sh
+# This script is used to build the image that is used in the PR Check
+source "$CICD_ROOT"/build.sh
+
+# The contents of this script can be found at:
+# https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd/deploy_ephemeral_env.sh
+# This script is used to deploy the ephemeral environment for smoke tests.
+source "$CICD_ROOT"/deploy_ephemeral_env.sh
+
+# Run smoke tests using a ClowdJobInvocation (preferred)
+# The contents of this script can be found at:
+# https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd/cji_smoke_test.sh
+source "$CICD_ROOT"/cji_smoke_test.sh
+
+# Post a comment with test run IDs to the PR
+# The contents of this script can be found at:
+# https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd/post_test_results.sh
+source "$CICD_ROOT"/post_test_results.sh
