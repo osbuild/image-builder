667e6efd6c31d03c4a2c51bd297ee6844c2ea5ad
Jakub Rusz <jrusz@redhat.com>
2022-12-13 13:17:44 +0100

    openshift-startup: remove obsolete condition
    We are now using the same template for crc and ephemeral.

--- distribution/openshift-startup.sh
+++ distribution/openshift-startup.sh
@@ -4,11 +4,6 @@ set -euo pipefail
 if [[ -z "${KUBERNETES_PORT:-}" ]]; then
     echo "Starting image-builder inside container..."
 # we don't use cloudwatch in ephemeral environment for now
-elif [[ "${CLOWDER_ENABLED:=false}" == "true" ]]; then
-    echo "Starting image-builder inside ephemeral environment..."
-    echo "Composer URL: ${COMPOSER_URL}"
-    echo "Composer token URL: ${COMPOSER_TOKEN_URL}"
-    echo "Distributions dir: ${DISTRIBUTIONS_DIR}"
 else
     echo "Starting image-builder inside OpenShift..."
     echo "Cloudwatch: ${CW_LOG_GROUP} in ${CW_AWS_REGION}"
