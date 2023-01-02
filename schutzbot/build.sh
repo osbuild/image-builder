3706c072728a4c83a2989ca0cd71bdaffa12fb1c
Diaa Sami <disami@redhat.com>
2023-01-02 16:39:59 +0100

    ci: update tests and containers to support aarch64

--- schutzbot/build.sh
+++ schutzbot/build.sh
@@ -7,11 +7,12 @@ function greenprint {
 }
 
 # Build the container and push to quay
+ARCH=$(uname -m)
 QUAY_REPO_URL="quay.io/osbuild/image-builder-test"
-QUAY_REPO_TAG="${CI_PIPELINE_ID}"
+QUAY_REPO_TAG="${CI_PIPELINE_ID}-$ARCH"
 
 sudo dnf install -y podman
 greenprint "🎁 Building container"
 sudo podman build --label="quay.expires-after=1w" --security-opt "label=disable" -t image-builder-"${QUAY_REPO_TAG}" -f distribution/Dockerfile-ubi .
 greenprint "🚀 Pushing container to test registry"
-sudo podman push --creds "${V2_QUAY_USERNAME}":"${V2_QUAY_PASSWORD}" image-builder-"${CI_PIPELINE_ID}" "${QUAY_REPO_URL}":"${QUAY_REPO_TAG}"
+sudo podman push --creds "${V2_QUAY_USERNAME}":"${V2_QUAY_PASSWORD}" image-builder-"${QUAY_REPO_TAG}" "${QUAY_REPO_URL}":"${QUAY_REPO_TAG}"
