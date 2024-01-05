e611f47ff0260be0059de918de47431597934c1d
Paweł Poławski <pawel.polawski@gmail.com>
2024-01-05 12:53:43 +0100

    Tests: Unify HTTP error codes used in test cases
    Some test cases used go literals as a HTTP error code,
    other uses digital const values. This commit unifies
    test to use only go defines for HTTP error codes.

--- internal/v1/server_identity_test.go
+++ internal/v1/server_identity_test.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -21,31 +22,31 @@ func TestIdentity(t *testing.T) {
 
 	t.Run("VerifyIdentityHeaderMissing", func(t *testing.T) {
 		respStatusCode, body := tutils.GetResponseBody(t, "http://localhost:8086/api/image-builder/v1/version", nil)
-		require.Equal(t, 400, respStatusCode)
+		require.Equal(t, http.StatusBadRequest, respStatusCode)
 		require.Contains(t, body, "missing x-rh-identity header")
 	})
 
 	t.Run("Valid authstring", func(t *testing.T) {
 		respStatusCode, _ := tutils.GetResponseBody(t, "http://localhost:8086/api/image-builder/v1/version", &tutils.AuthString0)
-		require.Equal(t, 200, respStatusCode)
+		require.Equal(t, http.StatusOK, respStatusCode)
 	})
 
 	t.Run("Valid authstring without entitlements", func(t *testing.T) {
 		respStatusCode, _ := tutils.GetResponseBody(t, "http://localhost:8086/api/image-builder/v1/version", &tutils.AuthString0WithoutEntitlements)
-		require.Equal(t, 200, respStatusCode)
+		require.Equal(t, http.StatusOK, respStatusCode)
 	})
 
 	t.Run("BogusAuthString", func(t *testing.T) {
 		auth := "notbase64"
 		respStatusCode, body := tutils.GetResponseBody(t, "http://localhost:8086/api/image-builder/v1/version", &auth)
-		require.Equal(t, 400, respStatusCode)
+		require.Equal(t, http.StatusBadRequest, respStatusCode)
 		require.Contains(t, body, "unable to b64 decode x-rh-identity header")
 	})
 
 	t.Run("BogusBase64AuthString", func(t *testing.T) {
 		auth := "dGhpcyBpcyBkZWZpbml0ZWx5IG5vdCBqc29uCg=="
 		respStatusCode, body := tutils.GetResponseBody(t, "http://localhost:8086/api/image-builder/v1/version", &auth)
-		require.Equal(t, 400, respStatusCode)
+		require.Equal(t, http.StatusBadRequest, respStatusCode)
 		require.Contains(t, body, "does not contain valid JSON")
 	})
 
@@ -53,14 +54,14 @@ func TestIdentity(t *testing.T) {
 		// AccoundNumber equals ""
 		auth := tutils.GetCompleteBase64Header("000000")
 		respStatusCode, _ := tutils.GetResponseBody(t, "http://localhost:8086/api/image-builder/v1/version", &auth)
-		require.Equal(t, 200, respStatusCode)
+		require.Equal(t, http.StatusOK, respStatusCode)
 	})
 
 	t.Run("EmptyOrgID", func(t *testing.T) {
 		// OrgID equals ""
 		auth := tutils.GetCompleteBase64Header("")
 		respStatusCode, body := tutils.GetResponseBody(t, "http://localhost:8086/api/image-builder/v1/version", &auth)
-		require.Equal(t, 400, respStatusCode)
+		require.Equal(t, http.StatusBadRequest, respStatusCode)
 		require.Contains(t, body, "invalid or missing org_id")
 	})
 }
