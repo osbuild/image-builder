d31d61c7c773b898bfa324ad1c5f51aeebba8a51
Gianluca Zuccarelli <gzuccare@redhat.com>
2024-03-21 11:28:18 +0000

    experiment

--- internal/db/db_trace.go
+++ internal/db/db_trace.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/sirupsen/logrus"
@@ -11,7 +13,18 @@ import (
 type dbTracer struct{}
 
 func (dt *dbTracer) TraceQueryStart(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryStartData) context.Context {
-	logrus.WithContext(ctx).Debugf("Executing SQL with args %v: %s", data.Args, data.SQL)
+	translated := make([]string, len(data.Args))
+	for k, v := range data.Args {
+		switch value := v.(type) {
+		case json.RawMessage:
+			translated[k] = string(value)
+		case []byte:
+			translated[k] = string(value)
+		default:
+			translated[k] = fmt.Sprintf("%v", value)
+		}
+	}
+	logrus.WithContext(ctx).Debugf("Executing SQL with args %v: %s", translated, data.SQL)
 	return ctx
 }
 
