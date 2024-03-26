22d322be32ff9a31c787d2bc36af56aa63793859
Florian Schüller <florian.schueller@redhat.com>
2024-03-26 16:47:21 +0100

    Makefile: implement "fully source containers" HMS-3883

--- distribution/openshift-startup.sh
+++ distribution/openshift-startup.sh
@@ -3,6 +3,12 @@ set -euo pipefail
 
 if [[ -z "${KUBERNETES_PORT:-}" ]]; then
     echo "Starting image-builder inside container..."
+    if [[ -n "${GODEBUG_PORT:-}" ]]; then
+      echo "With golang debugger enabled on port ${GODEBUG_PORT} ..."
+      echo "NOTE: you HAVE to attach the debugger NOW otherwise the image-builder-backend will not continue running"
+      /usr/bin/dlv "--listen=:${GODEBUG_PORT}" --headless=true --api-version=2 exec /app/image-builder -- -v
+      exit $?
+    fi
 # we don't use cloudwatch in ephemeral environment for now
 elif [[ "${CLOWDER_ENABLED:=false}" == "true" ]]; then
     echo "Starting image-builder inside ephemeral environment..."
