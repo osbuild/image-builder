93e03aa6e907587559d082924c886721fa1c4899
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2024-04-02 17:35:12 +0200

    clients: add content-sources client

---
+++ internal/clients/content_sources/client.go
@@ -0,0 +1,90 @@
+//go:generate go run -mod=mod github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config client.cfg.yaml content-sources.v1.json
+
+package content_sources
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/redhatinsights/identity"
+)
+
+type ContentSourcesClient struct {
+	url    *url.URL
+	client *http.Client
+}
+
+type ContentSourcesClientConfig struct {
+	URL string
+}
+
+func NewClient(conf ContentSourcesClientConfig) (*ContentSourcesClient, error) {
+	csURL, err := url.Parse(conf.URL)
+	if err != nil {
+		return nil, err
+	}
+	csc := ContentSourcesClient{
+		url:    csURL,
+		client: &http.Client{},
+	}
+
+	return &csc, nil
+}
+
+func (csc *ContentSourcesClient) request(method, url string, headers map[string]string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+
+	return csc.client.Do(req)
+}
+
+// returns ApiRepositoryCollectionResponse
+func (csc *ContentSourcesClient) GetRepositories(ctx context.Context, repoURLs []string, external bool) (*http.Response, error) {
+	id, ok := identity.GetIdentityHeader(ctx)
+	if !ok {
+		return nil, fmt.Errorf("Unable to get identity from context")
+	}
+
+	csReposURL := csc.url.JoinPath("repositories/")
+	queryValues := csReposURL.Query()
+	queryValues.Add("url", strings.Join(repoURLs, ","))
+	if external {
+		queryValues.Add("origin", "external")
+	} else {
+		queryValues.Add("origin", "red_hat")
+	}
+	csReposURL.RawQuery = queryValues.Encode()
+
+	return csc.request("GET", csReposURL.String(), map[string]string{
+		"x-rh-identity": id,
+	}, nil)
+}
+
+// returns ApiListSnapshotByDateResponse
+func (csc *ContentSourcesClient) GetSnapshotsForDate(ctx context.Context, body ApiListSnapshotByDateRequest) (*http.Response, error) {
+	id, ok := identity.GetIdentityHeader(ctx)
+	if !ok {
+		return nil, fmt.Errorf("Unable to get identity from context")
+	}
+
+	buf, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return csc.request("POST", csc.url.JoinPath("snapshots", "for_date/").String(), map[string]string{
+		"x-rh-identity": id,
+	}, bytes.NewReader(buf))
+}
