d9ae47dc56efcd91486e460b1e32f802b7d37495
Lukas Zapletal <lzap+git@redhat.com>
2024-04-10 08:59:17 +0200

    clients: move go:generate comment into separate file

---
+++ internal/clients/provisioning/package.go
@@ -0,0 +1,4 @@
+//go:generate go run -mod=mod github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config client.cfg.yml provisioning.v1.yml
+
+// Generated OpenAPI clients for the Provisioning service.
+package provisioning
