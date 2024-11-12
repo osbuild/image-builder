6607e9ae296cffd1a61be5385d45255f9e40e6e6
Florian Schüller <florian.schueller@redhat.com>
2024-11-12 14:33:35 +0100

    tools/dbtest-entrypoint: document implementation detail

--- tools/dbtest-entrypoint.sh
+++ tools/dbtest-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# compile and execute instead of a plain
+# "go test", to have the correct working directory
+
 go test -c -tags=integration -o image-builder-db-test ./cmd/image-builder-db-test/
 ./image-builder-db-test
