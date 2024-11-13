e2bd675278d9319bf24517fe08c620759e645d7e
Florian Schüller <florian.schueller@redhat.com>
2024-11-13 10:52:34 +0100

    Makefile/GH-actions: stop running the migrations
    the migrations are run from within the tests anyway
    and the database is wiped after each tearDown also

--- tools/dbtest-run-migrations.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-TERN_MIGRATIONS_DIR=${TERN_MIGRATIONS_DIR:-internal/db/migrations-tern}
-
-# make path absolute to find migrations for sure
-TERN_MIGRATIONS_DIR=$(realpath "$TERN_MIGRATIONS_DIR" --relative-base="$(dirname "$0")")
-
-"$(go env GOPATH)"/bin/tern migrate -m "$TERN_MIGRATIONS_DIR"
