95032691fd973eeb8db19c0b0295db4aea7670d3
Michael Vogt <michael.vogt@gmail.com>
2024-11-27 09:25:00 +0100

    filters: add new getOneImage() helper for manifest/build
    This commit adds a helper to find exactly a single image from a
    given disto/type/arch description. This will be used in `manifest`,
    `build` and potentially more. It is meant to support copy/paste
    from the `image-builder list-images` output, so: "distro:centos-9"
    is support just like "centos-9" (same for distro/imgType/arch).

--- cmd/image-builder/filters.go
+++ cmd/image-builder/filters.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/gobwas/glob"
+
 	"github.com/osbuild/images/pkg/distrofactory"
 	"github.com/osbuild/images/pkg/imagefilter"
 )
@@ -12,4 +17,46 @@ func newImageFilterDefault(dataDir string) (*imagefilter.ImageFilter, error) {
 		return nil, err
 	}
 	return imagefilter.New(fac, repos)
+}
+
+// should this be moved to images:imagefilter?
+func getOneImage(dataDir, distroName, imgTypeStr, archStr string) (*imagefilter.Result, error) {
+	imageFilter, err := newImageFilterDefault(dataDir)
+	if err != nil {
+		return nil, err
+	}
+	// strip prefixes to make ib copy/paste friendly when pasting output
+	// from "list-images"
+	distroName = strings.TrimPrefix(distroName, "distro:")
+	imgTypeStr = strings.TrimPrefix(imgTypeStr, "type:")
+	archStr = strings.TrimPrefix(archStr, "arch:")
+
+	// error early when globs are used
+	for _, s := range []string{distroName, imgTypeStr, archStr} {
+		if glob.QuoteMeta(s) != s {
+			return nil, fmt.Errorf("cannot use globs in %q when getting a single image", s)
+		}
+	}
+
+	filterExprs := []string{
+		fmt.Sprintf("distro:%s", distroName),
+		fmt.Sprintf("arch:%s", archStr),
+		fmt.Sprintf("type:%s", imgTypeStr),
+	}
+	filteredResults, err := imageFilter.Filter(filterExprs...)
+	if err != nil {
+		return nil, err
+	}
+	switch len(filteredResults) {
+	case 0:
+		return nil, fmt.Errorf("cannot find image for: distro:%q type:%q arch:%q", distroName, imgTypeStr, archStr)
+	case 1:
+		return &filteredResults[0], nil
+	default:
+		// This condition should never be hit in practise as we
+		// disallow globs above.
+		// XXX: imagefilter.Result should have a String() method so
+		// that this output can actually show the results
+		return nil, fmt.Errorf("internal error: found %v results for %q %q %q", len(filteredResults), distroName, imgTypeStr, archStr)
+	}
 }
