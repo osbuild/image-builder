26033c735abfc4f34155445b809e12739e5bf4ed
Michael Vogt <michael.vogt@gmail.com>
2025-01-09 16:12:09 +0100

    repos: lookup <buildin>, /{etc,usr/share}/image-builder/repositories
    This commit switches the default repositories to use the build-in
    ones from images PR#1112. It is still possible to override using
    `/etc/image-builder` and `/usr/share/image-builder` or `--datadir`.
    
    This is implicitly tested via the container test that no longer
    includes the files from osbuild-composer in the container.

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,23 @@
+import subprocess
+
+import pytest
+
+
 def pytest_configure(config):
     config.addinivalue_line(
         "markers", "images_integration"
     )
+
+
+# XXX: copied from bib
+@pytest.fixture(name="build_container", scope="session")
+def build_container_fixture():
+    """Build a container from the Containerfile and returns the name"""
+
+    container_tag = "image-builder-cli-test"
+    subprocess.check_call([
+        "podman", "build",
+        "-f", "Containerfile",
+        "-t", container_tag,
+    ])
+    return container_tag
