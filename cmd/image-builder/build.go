233db7b4c3234c87cd194890eb491b8b46131e2c
Michael Vogt <michael.vogt@gmail.com>
2025-01-20 11:35:01 +0100

    build: introduce `buildOptions`

--- cmd/image-builder/build.go
+++ cmd/image-builder/build.go
@@ -5,15 +5,24 @@ import (
 	"github.com/osbuild/images/pkg/osbuild"
 )
 
-func buildImage(res *imagefilter.Result, osbuildManifest []byte, osbuildStoreDir, outputDir string) error {
+type buildOptions struct {
+	OutputDir string
+	StoreDir  string
+}
+
+func buildImage(res *imagefilter.Result, osbuildManifest []byte, opts *buildOptions) error {
+	if opts == nil {
+		opts = &buildOptions{}
+	}
+
 	// XXX: support output filename via commandline (c.f.
 	//   https://github.com/osbuild/images/pull/1039)
-	if outputDir == "" {
-		outputDir = outputDirFor(res)
+	if opts.OutputDir == "" {
+		opts.OutputDir = outputDirFor(res)
 	}
 
 	// XXX: support stremaing via images/pkg/osbuild/monitor.go
-	_, err := osbuild.RunOSBuild(osbuildManifest, osbuildStoreDir, outputDir, res.ImgType.Exports(), nil, nil, false, osStderr)
+	_, err := osbuild.RunOSBuild(osbuildManifest, opts.StoreDir, opts.OutputDir, res.ImgType.Exports(), nil, nil, false, osStderr)
 	return err
 
 }
