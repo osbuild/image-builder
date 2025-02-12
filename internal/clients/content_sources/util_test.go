8ccff4805fd0bd58a45d47f2c5bf34e83cc9b8b2
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-02-12 19:02:38 +0100

    v1: fix getting baseurl for content sources upload repos
    These repositories don't have the `Url` set, only the
    `LatestSnapshotUrl` is set.

---
+++ internal/clients/content_sources/util_test.go
@@ -0,0 +1,39 @@
+package content_sources_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/osbuild/image-builder/internal/clients/content_sources"
+	"github.com/osbuild/image-builder/internal/common"
+)
+
+func TestGetBaseURL(t *testing.T) {
+	url, err := content_sources.GetBaseURL(content_sources.ApiRepositoryResponse{
+		Url:    common.ToPtr("someurl"),
+		Origin: common.ToPtr("external"),
+	})
+	require.NoError(t, err)
+	require.Equal(t, "someurl", url)
+
+	url, err = content_sources.GetBaseURL(content_sources.ApiRepositoryResponse{
+		Url:    common.ToPtr("someurl"),
+		Origin: common.ToPtr("red_hat"),
+	})
+	require.NoError(t, err)
+	require.Equal(t, "someurl", url)
+
+	url, err = content_sources.GetBaseURL(content_sources.ApiRepositoryResponse{
+		Url:               common.ToPtr("someurl"),
+		LatestSnapshotUrl: common.ToPtr("realurl"),
+		Origin:            common.ToPtr("upload"),
+	})
+	require.NoError(t, err)
+	require.Equal(t, "realurl", url)
+
+	_, err = content_sources.GetBaseURL(content_sources.ApiRepositoryResponse{
+		Uuid: common.ToPtr("d15a0c50-1549-4d04-bcb8-b3553576acd4"),
+	})
+	require.Error(t, err)
+}
