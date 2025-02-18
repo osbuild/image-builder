e62a8f96778c65ce2899fb7a7af6bf107c68242f
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-02-18 15:51:45 +0100

    clients/content_sources: fix baseurl for upload repositories
    Whenever snapshot URLs are used, they need to be used with the internal
    URL, as it's content hosted by content sources.

--- internal/clients/content_sources/util.go
+++ internal/clients/content_sources/util.go
@@ -2,15 +2,24 @@ package content_sources
 
 import (
 	"fmt"
+	"net/url"
 )
 
-func GetBaseURL(repo ApiRepositoryResponse) (string, error) {
+func GetBaseURL(repo ApiRepositoryResponse, csReposURL *url.URL) (string, error) {
 	if repo.Origin == nil {
 		return "", fmt.Errorf("unable to read origin from repository %s", *repo.Uuid)
 	}
 	switch *repo.Origin {
 	case "upload":
-		return *repo.LatestSnapshotUrl, nil
+		if csReposURL == nil {
+			return "", fmt.Errorf("upload repositories require a content sources URL")
+		}
+		// Snapshot URLs need to be replaced with the internal mtls URL
+		repoURL, err := url.Parse(*repo.LatestSnapshotUrl)
+		if err != nil {
+			return "", err
+		}
+		return csReposURL.JoinPath(repoURL.Path).String(), nil
 	case "external", "red_hat":
 		return *repo.Url, nil
 	}
