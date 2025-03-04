f5d0036747c1c8f88c72b8b976054802d939528b
Lukas Zapletal <lzap+git@redhat.com>
2025-03-04 07:24:54 +0100

    build: run 'go tidy' as the last prep step

--- go.mod
+++ go.mod
@@ -54,7 +54,6 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/oapi-codegen/oapi-codegen/v2 v2.4.1 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
--- tools/prepare-source.sh
+++ tools/prepare-source.sh
@@ -20,12 +20,6 @@ go version
 # Pin Go and toolchain versions at a reasonable version
 go get go@$GO_VERSION toolchain@$GO_VERSION
 
-# Update go.mod and go.sum:
-go mod tidy
-
-# Make sure dev tools are in go.mod
-go get github.com/oapi-codegen/oapi-codegen/v2@v$OAPI_VERSION
-
 # Ensure dev tools are installed
 test -e "$TOOLS_PATH/goimports" || GOBIN=$TOOLS_PATH go install golang.org/x/tools/cmd/goimports@latest
 ("$TOOLS_PATH/oapi-codegen" -version | grep "$OAPI_VERSION" >/dev/null) || GOBIN=$TOOLS_PATH go install github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v$OAPI_VERSION
@@ -36,3 +30,7 @@ GOBIN=$TOOLS_PATH go generate -x ./cmd/... ./internal/...
 # Reformat source (skip vendor/):
 "$TOOLS_PATH/goimports" -w ./internal ./cmd
 go fmt ./cmd/... ./internal/...
+
+# Update go.mod and go.sum as the last step
+go mod tidy
+
