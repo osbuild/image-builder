a1c9b45b83fe779117f4cc34bdfd583185556bc7
Simon de Vlieger <supakeen@redhat.com>
2025-07-14 21:05:25 +0200

    test: smoke test `--version`
    Only verify it returns the correct structure with (some) values in it.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- test/test_container.py
+++ test/test_container.py
@@ -4,6 +4,7 @@ import platform
 import subprocess
 
 import pytest
+import yaml
 
 
 @pytest.mark.parametrize("use_librepo", [False, True])
@@ -156,3 +157,19 @@ def test_container_manifest_seeded_is_the_same(build_container, use_seed_arg):
     else:
         print(cmd)
         assert len(manifests) == 3
+
+
+@pytest.mark.skipif(os.getuid() != 0, reason="needs root")
+def test_container_version_smoke(build_container):
+    output = subprocess.check_output([
+        "podman", "run",
+        "--privileged",
+        build_container,
+        "--version",
+    ])
+
+    ver_yaml = yaml.load(output, yaml.loader.SafeLoader)
+
+    assert ver_yaml["image-builder"]["version"] != ""
+    assert ver_yaml["image-builder"]["commit"] != ""
+    assert ver_yaml["image-builder"]["dependencies"]["images"] != ""
