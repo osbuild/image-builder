e96e85f83acb5a04a4674c130453f6ce68a6ef7c
Michael Vogt <michael.vogt@gmail.com>
2025-09-24 09:10:11 +0200

    test: check that we get the expected image types
    This test ensures that we have a minimal set of expected
    image types. This protects against the regression we had
    to fix in https://github.com/osbuild/images/pull/1895
    
    Co-Authored-By: Simon de Vlieger <supakeen@redhat.com>

--- test/test_container.py
+++ test/test_container.py
@@ -215,3 +215,59 @@ def test_container_manifest_bootc_build_container(build_container):
     cnt_deploy = [st for st in img_pipeline["stages"]
                   if st["type"] == "org.osbuild.bootc.install-to-filesystem"][0]
     assert cnt_deploy["options"]["target-imgref"] == "quay.io/centos-bootc/centos-bootc:stream9"
+
+
+def test_container_has_expected_images_centos(build_container):
+    """
+    Ensure that image types that are built in by CentOS are available
+    and do not disappear from the list. See:
+    https://gitlab.com/redhat/centos-stream/release-engineering/releng-tools/-/blob/master/scripts/images-build-gen2.py
+    """
+
+    output = subprocess.check_output(podman_run + [
+        build_container,
+        "list",
+    ], text=True)
+
+    type_arch = {
+        "tar": ["aarch64", "x86_64", "ppc64le", "s390x"],
+        "qcow2": ["aarch64", "x86_64", "ppc64le", "s390x"],
+        "ec2": ["x86_64", "aarch64"],
+        "azure": ["x86_64", "aarch64"],
+        "wsl": ["x86_64", "aarch64"],
+        "vagrant-libvirt": ["x86_64"],
+        "vagrant-virtualbox": ["x86_64"],
+        "image-installer": ["x86_64", "aarch64"],
+    }
+
+    for distro in ["centos-9", "centos-10"]:
+        for type_, arches in type_arch.items():
+            for arch in arches:
+                assert f"{distro} type:{type_} arch:{arch}" in output
+
+
+def test_container_has_expected_images_fedora(build_container):
+    """
+    Ensure that image types that are built in by Fedora are available
+    and do not disappear from the list. See:
+    https://pagure.io/pungi-fedora/blob/main/f/fedora.conf
+    and
+    https://pagure.io/fedora-iot/pungi-iot/blob/main/f/fedora-iot.conf
+    """
+
+    output = subprocess.check_output(podman_run + [
+        build_container,
+        "list",
+    ], text=True)
+
+    type_arch = {
+        "minimal-raw-xz": ["aarch64"],
+        "iot-raw-xz": ["x86_64", "aarch64"],
+        "iot-installer": ["x86_64", "aarch64"],
+        "iot-simplified-installer": ["x86_64", "aarch64"],
+    }
+
+    for distro in ["fedora-42", "fedora-43", "fedora-44"]:
+        for type_, arches in type_arch.items():
+            for arch in arches:
+                assert f"{distro} type:{type_} arch:{arch}" in output
