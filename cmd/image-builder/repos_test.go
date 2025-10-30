c3f98ba890c0e0d28851ebb5da85a0be2c07ff24
Michael Vogt <michael.vogt@gmail.com>
2025-10-30 09:59:32 +0100

    main: fix missing append of `repositories` when using --data-dirs
    This commit fixes the missing appending of `repositories` when
    `--data-dirs` is used. It also improves the tests a bit to
    ensure this won't happen again.
    
    Thanks to Achilleas for finding this!
    
    This was missing when we did
    https://github.com/osbuild/image-builder-cli/pull/128

--- cmd/image-builder/repos_test.go
+++ cmd/image-builder/repos_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/osbuild/images/pkg/rpmmd"
 )
@@ -16,7 +19,7 @@ func TestParseRepoURLsHappy(t *testing.T) {
 		"https://example.com/repo",
 	}, "forced")
 	assert.NoError(t, err)
-	assert.Equal(t, cfg, []rpmmd.RepoConfig{
+	assert.Equal(t, []rpmmd.RepoConfig{
 		{
 			Id:           "forced-repo-0",
 			Name:         "forced repo#0 /path/to/repo",
@@ -31,7 +34,7 @@ func TestParseRepoURLsHappy(t *testing.T) {
 			CheckGPG:     &checkGPG,
 			CheckRepoGPG: &checkGPG,
 		},
-	})
+	}, cfg)
 }
 
 func TestParseExtraRepoSad(t *testing.T) {
@@ -40,4 +43,55 @@ func TestParseExtraRepoSad(t *testing.T) {
 
 	_, err = parseRepoURLs([]string{"https://example.com", "/just/a/path"}, "forced")
 	assert.EqualError(t, err, `scheme missing in "/just/a/path", please prefix with e.g. file:// or https://`)
+}
+
+func TestNewRepoRegistryImplSmoke(t *testing.T) {
+	registry, err := newRepoRegistryImpl("", nil)
+	require.NoError(t, err)
+	repos, err := registry.DistroHasRepos("rhel-10.2", "x86_64")
+	require.NoError(t, err)
+	assert.True(t, len(repos) > 0)
+}
+
+func TestNewRepoRegistryImplExtraReposGetAppended(t *testing.T) {
+	registry, err := newRepoRegistryImpl("", []string{"https://example.com/my/repo"})
+	require.NoError(t, err)
+	repos, err := registry.DistroHasRepos("rhel-10.2", "x86_64")
+	require.NoError(t, err)
+	assert.Equal(t, repos[len(repos)-1].BaseURLs[0], "https://example.com/my/repo")
+}
+
+func TestNewRepoRegistryImplDatadir(t *testing.T) {
+	// prereq test: no testdistro-1 in the default repos
+	registry, err := newRepoRegistryImpl("", nil)
+	require.NoError(t, err)
+	assert.NotContains(t, registry.ListDistros(), "testdistro-1")
+	_, err = registry.DistroHasRepos("testdistro-1", "x86_64")
+	require.EqualError(t, err, `requested repository not found: for distribution "testdistro-1"`)
+
+	// create a custom datadir with testdistro-1.json, the basefilename
+	// must match a distro nameVer
+	dataDir := t.TempDir()
+	repoFile := filepath.Join(dataDir, "repositories", "testdistro-1.json")
+	err = os.Mkdir(filepath.Dir(repoFile), 0755)
+	require.NoError(t, err)
+	repoContents := `{
+	"x86_64": [
+		{
+			"name": "testdistro-1-repo",
+			"baseurl": "https://example.com/test/test/distro/1"
+		}
+	]
+}
+`
+	err = os.WriteFile(repoFile, []byte(repoContents), 0644)
+	require.NoError(t, err)
+
+	// and ensure we have testdistro-1 now
+	registry, err = newRepoRegistryImpl(dataDir, nil)
+	require.NoError(t, err)
+	repos, err := registry.DistroHasRepos("testdistro-1", "x86_64")
+	require.NoError(t, err)
+	assert.Len(t, repos, 1)
+	assert.Equal(t, repos[0].Name, "testdistro-1-repo")
 }
