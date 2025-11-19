6656f8bb8dac13432845fdf16d5348a547f24800
Michael Vogt <michael.vogt@gmail.com>
2025-11-19 12:23:10 +0100

    bib: update go imports

--- cmd/bootc-image-builder/mtls_test.go
+++ cmd/bootc-image-builder/mtls_test.go
@@ -7,9 +7,10 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/osbuild/images/pkg/rpmmd"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/osbuild/images/pkg/rpmmd"
 )
 
 type fakeFileReader struct {
