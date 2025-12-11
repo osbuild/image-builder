d60b93e60db0d01f1291481d054600b004342e49
Michael Vogt <michael.vogt@gmail.com>
2025-12-11 17:23:22 +0100

    test: add integration test for test_rhel_bootc_anaconda_iso
    This commit adds an integration test for the fix that bootc anaconda
    ISO secrets must be read while the container is mounted.

--- test/test_rhel.py
+++ test/test_rhel.py
@@ -46,3 +46,26 @@ def test_build_can_build_tar(build_container):
         "tar",
         "--distro", "centos-10",
     ])
+
+
+@pytest.mark.skipif(os.getuid() != 0, reason="needs root")
+@pytest.mark.skipif(not os.getenv("RHEL_REGISTRY_USER"), reason="needs registry secrets")
+def test_rhel_bootc_anaconda_iso(tmp_path):
+    rhel_registry_user = os.getenv("RHEL_REGISTRY_USER")
+    rhel_registry_secret = os.getenv("RHEL_REGISTRY_TOKEN")
+    bootc_ref = "registry.redhat.io/rhel10/rhel-bootc:latest"
+    subprocess.run([
+        "podman",
+        "login", "registry.redhat.io",
+        "--username", rhel_registry_user,
+        "--password-stdin",
+    ], input=rhel_registry_secret, text=True, check=True)
+    subprocess.check_call(["podman", "pull", bootc_ref])
+    bib = tmp_path / "bootc-image-builder"
+    subprocess.check_call(["go", "build", "-o", f"{bib}", "./cmd/image-builder"])
+    subprocess.check_call([
+        os.fspath(bib),
+        "manifest",
+        "--type", "anaconda-iso",
+        bootc_ref,
+    ])
