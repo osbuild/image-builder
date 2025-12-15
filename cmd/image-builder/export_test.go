71738bfecedebe95212db90ffc40c940df597f1d
Simon de Vlieger <supakeen@redhat.com>
2025-12-15 10:43:14 +0100

    many: `force-repo-dir` instead of `force-data-dir`
    Previously I deprecated `data-dir` and renamed it to `force-data-dir` to
    have consistent naming for options that overwrite internal state instead
    of appending to it (arguments that start with `force-` are overwrites,
    arguments that start with `extra-` are merges).
    
    With `force-data-dir` we require a `repositories` sub directory. After
    some discussion we came to the idea to instead of enforcing a directory
    layout on the user we want to be able to point directly at a directory
    of repositories.
    
    This commit deprecates `force-data-dir` and introduces `force-repo-dir`.
    Both write to the same internal variable.
    
    We also allow repository files to live *either* in a `repositories`
    subdirectory to keep backwards compatibility for any of the arguments or
    to live directly at the top level of the passed directory.
    
    We show a deprecation warning if a `repositories` sub directory exists
    and inform users that the `repositories` subdirectory will not be wanted
    in the future and how to resolve the emitted warning.
    
    This option will be used together with the future introduction of
    `force-defs-dir` to handle definitions (and its `extra-defs-dir` counter
    part) unifying all options into a consistent naming scheme.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- cmd/image-builder/export_test.go
+++ cmd/image-builder/export_test.go
@@ -49,9 +49,9 @@ func MockOsStderr(new io.Writer) (restore func()) {
 
 func MockNewRepoRegistry(f func() (*reporegistry.RepoRegistry, error)) (restore func()) {
 	saved := newRepoRegistry
-	newRepoRegistry = func(dataDir string, extraRepos []string) (*reporegistry.RepoRegistry, error) {
-		if dataDir != "" {
-			panic(fmt.Sprintf("cannot use custom dataDir %v in mock", dataDir))
+	newRepoRegistry = func(repoDir string, extraRepos []string) (*reporegistry.RepoRegistry, error) {
+		if repoDir != "" {
+			panic(fmt.Sprintf("cannot use custom repoDir %v in mock", repoDir))
 		}
 		return f()
 	}
--- cmd/image-builder/main.go
+++ cmd/image-builder/main.go
@@ -67,7 +67,7 @@ func cmdListImages(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
-	dataDir, err := cmd.Flags().GetString("force-data-dir")
+	repoDir, err := cmd.Flags().GetString("force-repo-dir")
 	if err != nil {
 		return err
 	}
@@ -76,7 +76,7 @@ func cmdListImages(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	return listImages(dataDir, extraRepos, format, filter)
+	return listImages(repoDir, extraRepos, format, filter)
 }
 
 func ostreeImageOptions(cmd *cobra.Command) (*ostree.ImageOptions, error) {
@@ -146,7 +146,7 @@ func cmdManifestWrapper(pbar progress.ProgressBar, cmd *cobra.Command, args []st
 	if wrapperOpts == nil {
 		wrapperOpts = &cmdManifestWrapperOptions{}
 	}
-	dataDir, err := cmd.Flags().GetString("force-data-dir")
+	repoDir, err := cmd.Flags().GetString("force-repo-dir")
 	if err != nil {
 		return nil, err
 	}
@@ -294,7 +294,7 @@ func cmdManifestWrapper(pbar progress.ProgressBar, cmd *cobra.Command, args []st
 		forceRepos = []string{"https://example.com/not-used"}
 	} else {
 		repoOpts := &repoOptions{
-			DataDir:    dataDir,
+			RepoDir:    repoDir,
 			ExtraRepos: extraRepos,
 			ForceRepos: forceRepos,
 		}
@@ -331,8 +331,7 @@ func cmdManifestWrapper(pbar progress.ProgressBar, cmd *cobra.Command, args []st
 	if opts.ManifestgenOptions.UseBootstrapContainer {
 		fmt.Fprintf(os.Stderr, "WARNING: using experimental cross-architecture building to build %q\n", img.ImgType.Arch().Name())
 	}
-
-	err = generateManifest(dataDir, extraRepos, img, w, opts)
+	err = generateManifest(repoDir, extraRepos, img, w, opts)
 	return img, err
 }
 
@@ -467,7 +466,7 @@ func cmdBuild(cmd *cobra.Command, args []string) error {
 
 func cmdDescribeImg(cmd *cobra.Command, args []string) error {
 	// XXX: boilderplate identical to cmdManifest() above
-	dataDir, err := cmd.Flags().GetString("force-data-dir")
+	repoDir, err := cmd.Flags().GetString("force-repo-dir")
 	if err != nil {
 		return err
 	}
@@ -489,7 +488,7 @@ func cmdDescribeImg(cmd *cobra.Command, args []string) error {
 	}
 
 	imgTypeStr := args[0]
-	res, err := getOneImage(distroStr, imgTypeStr, archStr, &repoOptions{DataDir: dataDir})
+	res, err := getOneImage(distroStr, imgTypeStr, archStr, &repoOptions{RepoDir: repoDir})
 	if err != nil {
 		return err
 	}
@@ -500,7 +499,10 @@ func cmdDescribeImg(cmd *cobra.Command, args []string) error {
 func normalizeRootArgs(_ *pflag.FlagSet, name string) pflag.NormalizedName {
 	switch name {
 	case "data-dir":
-		name = "force-data-dir"
+		name = "force-repo-dir"
+		break
+	case "force-data-dir":
+		name = "force-repo-dir"
 		break
 	}
 
@@ -536,10 +538,12 @@ operating systems like Fedora, CentOS and RHEL with easy customizations support.
 	}
 
 	rootCmd.Flags().Bool("version", false, "Print version information and exit")
-	var forceDataDir string
-	rootCmd.PersistentFlags().StringVar(&forceDataDir, "force-data-dir", "", `Override the default data directory for e.g. custom repositories/*.json data`)
-	rootCmd.PersistentFlags().StringVar(&forceDataDir, "data-dir", "", `Override the default data directory for e.g. custom repositories/*.json data`)
-	rootCmd.PersistentFlags().MarkDeprecated("data-dir", `Use --force-data-dir instead`)
+	var forceRepoDir string
+	rootCmd.PersistentFlags().StringVar(&forceRepoDir, "force-repo-dir", "", "Override the default repository search path for custom repository files")
+	rootCmd.PersistentFlags().StringVar(&forceRepoDir, "force-data-dir", "", `Override the default data directory for e.g. custom repositories/*.json data`)
+	rootCmd.PersistentFlags().MarkDeprecated("force-data-dir", `Use --force-repo-dir instead`)
+	rootCmd.PersistentFlags().StringVar(&forceRepoDir, "data-dir", "", `Override the default data directory for e.g. custom repositories/*.json data`)
+	rootCmd.PersistentFlags().MarkDeprecated("data-dir", `Use --force-repo-dir instead`)
 	rootCmd.PersistentFlags().StringArray("extra-repo", nil, `Add an extra repository during build (will *not* be gpg checked and not be part of the final image)`)
 	rootCmd.PersistentFlags().StringArray("force-repo", nil, `Override the base repositories during build (these will not be part of the final image)`)
 	rootCmd.PersistentFlags().String("output-dir", "", `Put output into the specified directory`)
