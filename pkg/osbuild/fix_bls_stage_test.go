49e348051ff1eaadbec42d3edb7fec229f484676
Simon de Vlieger <cmdr@supakeen.com>
2026-04-21 10:08:36 +0200

    osbuild: fix-bls without a prefix
    A new toggle in the `osbuild` `fix-bls` stage allows us to replace paths
    that don't have a prefix. When BLS files are created by `90-loaderentry`
    there won't be any other prefix than `/boot` which without setting this
    flag wouldn't be replaced.
    
    Replacement of `/boot` is necessary when `/boot` is a mountpoint (either
    XBOOTLDR, or on the ESP directly.
    
    Signed-off-by: Simon de Vlieger <cmdr@supakeen.com>

--- pkg/osbuild/fix_bls_stage_test.go
+++ pkg/osbuild/fix_bls_stage_test.go
@@ -1,9 +1,13 @@
 package osbuild
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/osbuild/images/internal/common"
 )
 
 func TestNewFixBLSStage(t *testing.T) {
@@ -13,4 +17,50 @@ func TestNewFixBLSStage(t *testing.T) {
 	}
 	actualStage := NewFixBLSStage(&FixBLSStageOptions{})
 	assert.Equal(t, expectedStage, actualStage)
+}
+
+func TestFixBLSStageJSONRequireBootPrefix(t *testing.T) {
+	opts := &FixBLSStageOptions{
+		RequireBootPrefix: common.ToPtr(false),
+	}
+	stage := NewFixBLSStage(opts)
+	data, err := json.MarshalIndent(stage, "", "  ")
+	require.NoError(t, err)
+
+	assert.Equal(t, `{
+  "type": "org.osbuild.fix-bls",
+  "options": {
+    "require_boot_prefix": false
+  }
+}`, string(data))
+}
+
+func TestFixBLSStageJSONPrefixAndRequireBootPrefix(t *testing.T) {
+	opts := &FixBLSStageOptions{
+		Prefix:            common.ToPtr(""),
+		RequireBootPrefix: common.ToPtr(false),
+	}
+	stage := NewFixBLSStage(opts)
+	data, err := json.MarshalIndent(stage, "", "  ")
+	require.NoError(t, err)
+
+	assert.Equal(t, `{
+  "type": "org.osbuild.fix-bls",
+  "options": {
+    "prefix": "",
+    "require_boot_prefix": false
+  }
+}`, string(data))
+}
+
+func TestFixBLSStageJSONOmitEmpty(t *testing.T) {
+	opts := &FixBLSStageOptions{}
+	stage := NewFixBLSStage(opts)
+	data, err := json.MarshalIndent(stage, "", "  ")
+	require.NoError(t, err)
+
+	assert.Equal(t, `{
+  "type": "org.osbuild.fix-bls",
+  "options": {}
+}`, string(data))
 }
