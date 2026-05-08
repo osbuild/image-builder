583adad346a745b1c8dde4c65a22926b060d4822
Brian C. Lane <bcl@redhat.com>
2026-05-08 14:04:01 -0700

    container_based_iso: Use bootc container directly
    This changes to using the bootc container directly instead of the
    deployed filesystem. It builds on top of BootcRootFS (also used by the
    bootc PXE tar image type). It uses the iso and installer customizations
    from the bootc distro YAML for setting up the rootfs compression and
    optional kernel arguments.
    
    It also uses the common bootloaders functions which should make it
    easier to eventually add support for other platforms.
    
    Related: HMS-10627

--- pkg/image/container_based_iso.go
+++ pkg/image/container_based_iso.go
@@ -8,6 +8,7 @@ import (
 	"github.com/osbuild/image-builder/pkg/container"
 	"github.com/osbuild/image-builder/pkg/disk"
 	"github.com/osbuild/image-builder/pkg/manifest"
+	"github.com/osbuild/image-builder/pkg/osbuild"
 	"github.com/osbuild/image-builder/pkg/platform"
 	"github.com/osbuild/image-builder/pkg/runner"
 )
@@ -15,31 +16,23 @@ import (
 type ContainerBasedIso struct {
 	Base
 
+	PartitionTable *disk.PartitionTable
+
+	// Customizations
+	OSCustomizations        manifest.OSCustomizations
+	DiskCustomizations      manifest.DiskCustomizations
+	ISOCustomizations       manifest.ISOCustomizations
+	InstallerCustomizations manifest.InstallerCustomizations
+
+	// Kernel version from the container, used to copy it into the PXE tar tree
+	KernelVersion string
+
 	// Container source for the OS tree
 	ContainerSource container.SourceSpec
 
-	// PayloadContainer is an optional container to embed in the image's
-	// container storage (for bootc installer scenarios where the payload
-	// container needs to be available at install time).
-	PayloadContainer *container.SourceSpec
+	KernelOpts []string
 
-	Product string
-	Version string
-	Release string
-
-	ISOLabel string
-
-	RootfsCompression string
-	RootfsType        manifest.ISORootfsType
-	RootfsExcludes    []string
-
-	KernelPath    string
-	KernelOpts    []string
-	InitramfsPath string
-
-	Grub2MenuDefault *int
-	Grub2MenuTimeout *int
-
+	// Custom ISO bootloader menus override the default menus
 	Grub2MenuEntries []manifest.ISOGrub2MenuEntry
 }
 
@@ -47,7 +40,21 @@ func NewContainerBasedIso(platform platform.Platform, filename string, container
 	return &ContainerBasedIso{
 		Base:            NewBase("container-based-iso", platform, filename),
 		ContainerSource: container,
+		DiskCustomizations: manifest.DiskCustomizations{
+			MountConfiguration: osbuild.MOUNT_CONFIGURATION_NONE, // default to no mount config for PXE images
+		},
 	}
+}
+
+// Bootloaders returns the list of configured bootloaders for the platform
+func (img *ContainerBasedIso) Bootloaders(buildPipeline manifest.Build, kernelOpts []string) []manifest.ISOBootloader {
+	ibo := ISOBootloaders{
+		InstallerCustomizations: &img.InstallerCustomizations,
+		ISOCustomizations:       &img.ISOCustomizations,
+		Custom:                  img.Grub2MenuEntries,
+	}
+
+	return ibo.Bootloaders(buildPipeline, img.platform, kernelOpts)
 }
 
 func (img *ContainerBasedIso) InstantiateManifestFromContainer(m *manifest.Manifest,
@@ -56,72 +63,58 @@ func (img *ContainerBasedIso) InstantiateManifestFromContainer(m *manifest.Manif
 	rng *rand.Rand) (*artifact.Artifact, error) {
 	cnts := []container.SourceSpec{img.ContainerSource}
 
-	kernelOpts := []string{}
-
-	if len(img.KernelOpts) > 0 {
-		kernelOpts = append(kernelOpts, img.KernelOpts...)
-	} else {
-		// org.osbuild.grub2.iso.legacy fails to run with empty kernel options, so let's use something harmless
-		// if we didn't have any specific kernelopts set
-		kernelOpts = []string{
-			fmt.Sprintf("root=live:CDLABEL=%s", img.ISOLabel),
-			"rd.live.image",
-			"quiet",
-			"rhgb",
-			"enforcing=0",
-		}
-	}
-
 	buildOptions := img.BuildOptions
 	if buildOptions == nil {
 		buildOptions = &manifest.BuildOptions{}
 	}
 	buildOptions.ContainerBuildable = true
 	buildPipeline := manifest.NewBuildFromContainer(m, runner, cnts, buildOptions)
-	osTreePipeline := manifest.NewOSFromContainer("os-tree", buildPipeline, &img.ContainerSource)
-	osTreePipeline.PayloadContainer = img.PayloadContainer
 
-	product := img.Product
-	if product == "" {
-		product = "OS"
+	rawImage := manifest.NewRawBootcImage(buildPipeline, containers, img.platform)
+	rawImage.PartitionTable = img.PartitionTable
+	rawImage.OSCustomizations = img.OSCustomizations
+	rawImage.DiskCustomizations = img.DiskCustomizations
+	rawImage.KernelVersion = img.KernelVersion
+
+	// Setup root filesystem so that dmsquash-live will boot it
+	rawImage.LiveBoot = true
+
+	rootfsPipeline := manifest.NewBootcRootFS(buildPipeline, rawImage, img.platform)
+	rootfsPipeline.ErofsOptions = img.ISOCustomizations.ErofsOptions
+	rootfsPipeline.RootfsType = img.ISOCustomizations.RootfsType
+	rootfsPipeline.RootfsExcludes = img.ISOCustomizations.RootfsExcludes
+
+	// Setup the boot args for a live iso with ostree rootfs
+	// The @OSTREE@ entry is substituted by the org.osbuild.ostree.grub2 stage
+	// after inspecting the rootfs.img for the ostree default boot target's path.
+	// this requires the ISOTree bool SetOSTREE to be true.
+	kernelOpts := []string{
+		// TODO escape the label (or make sure it is escaped earlier)
+		fmt.Sprintf("root=live:CDLABEL=%s", img.ISOCustomizations.Label),
+		"ostree=@OSTREE@",
+		"rd.live.image",
+		"fstab=no", // Prevents systemd-fstab-generator from making mount units
+		"quiet",
+		"rhgb",
 	}
-	version := img.Version
-	if version == "" {
-		version = img.Release
-	}
-	bootTreePipeline := manifest.NewEFIBootTree(buildPipeline, product, version)
-	bootTreePipeline.Platform = img.platform
-	bootTreePipeline.UEFIVendor = img.platform.GetUEFIVendor()
-	bootTreePipeline.ISOLabel = img.ISOLabel
-	bootTreePipeline.KernelOpts = kernelOpts
-	bootTreePipeline.MenuTimeout = img.Grub2MenuTimeout
-	bootTreePipeline.DisableTestEntry = true
-	bootTreePipeline.DisableTroubleshootingEntry = true
+	kernelOpts = append(kernelOpts, img.KernelOpts...)
+	kernelOpts = append(kernelOpts, img.InstallerCustomizations.KernelOptionsAppend...)
 
-	if img.Grub2MenuDefault != nil {
-		bootTreePipeline.DefaultMenu = *img.Grub2MenuDefault
-	}
+	// Setup the bootloaders
+	bootloaders := img.Bootloaders(buildPipeline, kernelOpts)
 
-	bootTreePipeline.MenuEntries = img.Grub2MenuEntries
-
-	isoTreePipeline := manifest.NewISOTree(buildPipeline, osTreePipeline, bootTreePipeline)
+	isoTreePipeline := manifest.NewISOTree(buildPipeline, rootfsPipeline, bootloaders)
 	isoTreePipeline.PartitionTable = disk.EFIBootPartitionTable(rng)
-	isoTreePipeline.Release = img.Release
-	isoTreePipeline.Product = img.Product
-	isoTreePipeline.Version = img.Version
-	isoTreePipeline.RootfsCompression = img.RootfsCompression
-	isoTreePipeline.RootfsType = img.RootfsType
-	isoTreePipeline.RootfsExcludes = img.RootfsExcludes
-	isoTreePipeline.KernelPath = img.KernelPath
-	isoTreePipeline.InitramfsPath = img.InitramfsPath
-	isoTreePipeline.KernelOpts = kernelOpts
+	isoTreePipeline.Release = img.InstallerCustomizations.Release
+	// Paths are relative to the rootfsPipeline tree root
+	isoTreePipeline.KernelPath = "vmlinuz"
+	isoTreePipeline.InitramfsPath = "initrd.img"
+	isoTreePipeline.RootfsPath = "rootfs.img"
+	// Mount the rootfs, get the ostree boot path, update grub.cfg ostree= entry
+	isoTreePipeline.SetOSTREE = true
+	isoTreePipeline.RootfsType = rootfsPipeline.RootfsType
 
-	isoCustomizations := manifest.ISOCustomizations{
-		Label:    img.ISOLabel,
-		BootType: manifest.Grub2ISOBoot,
-	}
-
-	isoPipeline := manifest.NewISO(buildPipeline, isoTreePipeline, isoCustomizations)
+	isoPipeline := manifest.NewISO(buildPipeline, isoTreePipeline, img.ISOCustomizations)
 	isoPipeline.SetFilename(img.filename)
 	artifact := isoPipeline.Export()
 
