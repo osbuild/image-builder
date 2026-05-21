234c82f9b7df035f97375b8b70cae903b7e6c7fb
Brian C. Lane <bcl@redhat.com>
2026-05-21 15:36:27 -0700

    osbuild: Bump osbuild version to v183
    This includes support for squashfs mount support and erofs mount support
    in BootcRootFS
    
    Related: HMS-10627

--- data/dependencies/deps_test.go
+++ data/dependencies/deps_test.go
@@ -8,5 +8,5 @@ import (
 )
 
 func TestMinimumOSBuildVersion(t *testing.T) {
-	assert.Equal(t, "180", dependencies.MinimumOSBuildVersion())
+	assert.Equal(t, "183", dependencies.MinimumOSBuildVersion())
 }
