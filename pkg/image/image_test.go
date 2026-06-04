1a6524bb2672986d417cb27c4fa7f315ac95bd4a
Simon de Vlieger <cmdr@supakeen.com>
2026-06-04 06:51:37 +0200

    image: test `GetCompressionPipeline`
    There were no tests for `GetCompressionPipeline` yet. Let's introduce
    one where we compress a tar archive.
    
    I want to fiddle with the compression pipelines in the next commits so
    it's a good idea to first test the current behavior.
    
    Signed-off-by: Simon de Vlieger <cmdr@supakeen.com>

---
+++ pkg/image/image_test.go
@@ -0,0 +1,53 @@
+package image_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/osbuild/image-builder/pkg/image"
+	"github.com/osbuild/image-builder/pkg/manifest"
+	"github.com/osbuild/image-builder/pkg/rpmmd"
+	"github.com/osbuild/image-builder/pkg/runner"
+)
+
+func makeGetCompressionPipelineInputs() (manifest.Build, manifest.FilePipeline) {
+	mf := manifest.New()
+	build := manifest.NewBuild(&mf, &runner.Fedora{Version: 41}, []rpmmd.RepoConfig{}, nil)
+	inputPipeline := manifest.NewTar(build, nil, "input")
+	return build, inputPipeline
+}
+
+func TestGetCompressionPipeline(t *testing.T) {
+	tests := []struct {
+		name             string
+		compression      string
+		expectedPipeline string
+	}{
+		{"xz", "xz", "xz"},
+		{"zstd", "zstd", "zstd"},
+		{"gzip", "gzip", "gzip"},
+		{"empty", "", ""},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			build, inputPipeline := makeGetCompressionPipelineInputs()
+			result := image.GetCompressionPipeline(tt.compression, build, inputPipeline)
+			require.NotNil(t, result)
+			if tt.expectedPipeline == "" {
+				assert.Equal(t, inputPipeline, result)
+			} else {
+				assert.Equal(t, tt.expectedPipeline, result.Name())
+			}
+		})
+	}
+}
+
+func TestGetCompressionPipelinePanicsOnUnknown(t *testing.T) {
+	build, inputPipeline := makeGetCompressionPipelineInputs()
+	assert.PanicsWithValue(t, `unsupported compression type "banana"`, func() {
+		image.GetCompressionPipeline("banana", build, inputPipeline)
+	})
+}
