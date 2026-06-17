osbuild/image-builder-cli

many: move to use the new github.com/osbuild/blueprint module
This commit moves ibcli over to use the new `osbuild/blueprint`
module for better compatibility with the composer and on-prem
blueprints.

internal/blueprintload/blueprintload.go
@@ -9,14 +9,18 @@ import (
 
 	"github.com/BurntSushi/toml"
 
-	"github.com/osbuild/images/pkg/blueprint"
+	// XXX: there should only be one importable blueprint, i.e.
+	// importing the external and passing it to images, if
+	// images needs its own it should not be public
+	externalBlueprint "github.com/osbuild/blueprint/pkg/blueprint"
+	imagesBlueprint "github.com/osbuild/images/pkg/blueprint"
 )
 
 // XXX: move this helper into images, share with bib
-func decodeToml(r io.Reader, what string) (*blueprint.Blueprint, error) {
+func decodeToml(r io.Reader, what string) (*externalBlueprint.Blueprint, error) {
 	dec := toml.NewDecoder(r)
 
-	var conf blueprint.Blueprint
+	var conf externalBlueprint.Blueprint
 	metadata, err := dec.Decode(&conf)
 	if err != nil {
 		return nil, fmt.Errorf("cannot decode %q: %w", what, err)
@@ -28,11 +32,11 @@ func decodeToml(r io.Reader, what string) (*blueprint.Blueprint, error) {
 	return &conf, nil
 }
 
-func decodeJson(r io.Reader, what string) (*blueprint.Blueprint, error) {
+func decodeJson(r io.Reader, what string) (*externalBlueprint.Blueprint, error) {
 	dec := json.NewDecoder(r)
 	dec.DisallowUnknownFields()
 
-	var conf blueprint.Blueprint
+	var conf externalBlueprint.Blueprint
 	if err := dec.Decode(&conf); err != nil {
 		return nil, fmt.Errorf("cannot decode %q: %w", what, err)
 	}
@@ -42,13 +46,13 @@ func decodeJson(r io.Reader, what string) (*blueprint.Blueprint, error) {
 	return &conf, nil
 }
 
-func Load(path string) (*blueprint.Blueprint, error) {
+func load(path string) (*externalBlueprint.Blueprint, error) {
 	var fp io.ReadCloser
 	var err error
 
 	switch path {
 	case "":
-		return &blueprint.Blueprint{}, nil
+		return &externalBlueprint.Blueprint{}, nil
 	case "-":
 		fp = os.Stdin
 	default:
@@ -67,4 +71,15 @@ func Load(path string) (*blueprint.Blueprint, error) {
 	default:
 		return nil, fmt.Errorf("unsupported file extension for %q (please use .toml or .json)", path)
 	}
+}
+
+func Load(path string) (*imagesBlueprint.Blueprint, error) {
+	externalBp, err := load(path)
+	if err != nil {
+		return nil, err
+	}
+	// XXX: make convert a method on "Blueprint"
+	// XXX2: make Convert() take a pointer
+	imagesBp := externalBlueprint.Convert(*externalBp)
+	return &imagesBp, nil
 }

osbuild/image-builder

internal/blueprintload/blueprintload.go
@@ -9,14 +9,17 @@ import (
 
 	"github.com/BurntSushi/toml"
 
-	"github.com/osbuild/blueprint/pkg/blueprint"
+	// XXX: there should only be one importable blueprint, i.e.
+	// importing the external and passing it to images, if
+	// images needs its own it should not be public
+	externalBlueprint "github.com/osbuild/blueprint/pkg/blueprint"
 )
 
 // XXX: move this helper into images, share with bib
-func decodeToml(r io.Reader, what string) (*blueprint.Blueprint, error) {
+func decodeToml(r io.Reader, what string) (*externalBlueprint.Blueprint, error) {
 	dec := toml.NewDecoder(r)
 
-	var conf blueprint.Blueprint
+	var conf externalBlueprint.Blueprint
 	metadata, err := dec.Decode(&conf)
 	if err != nil {
 		return nil, fmt.Errorf("cannot decode %q: %w", what, err)
@@ -28,11 +31,11 @@ func decodeToml(r io.Reader, what string) (*blueprint.Blueprint, error) {
 	return &conf, nil
 }
 
-func decodeJson(r io.Reader, what string) (*blueprint.Blueprint, error) {
+func decodeJson(r io.Reader, what string) (*externalBlueprint.Blueprint, error) {
 	dec := json.NewDecoder(r)
 	dec.DisallowUnknownFields()
 
-	var conf blueprint.Blueprint
+	var conf externalBlueprint.Blueprint
 	if err := dec.Decode(&conf); err != nil {
 		return nil, fmt.Errorf("cannot decode %q: %w", what, err)
 	}
@@ -42,13 +45,13 @@ func decodeJson(r io.Reader, what string) (*blueprint.Blueprint, error) {
 	return &conf, nil
 }
 
-func Load(path string) (*blueprint.Blueprint, error) {
+func load(path string) (*externalBlueprint.Blueprint, error) {
 	var fp io.ReadCloser
 	var err error
 
 	switch path {
 	case "":
-		return &blueprint.Blueprint{}, nil
+		return &externalBlueprint.Blueprint{}, nil
 	case "-":
 		fp = os.Stdin
 	default:
@@ -67,4 +70,15 @@ func Load(path string) (*blueprint.Blueprint, error) {
 	default:
 		return nil, fmt.Errorf("unsupported file extension for %q (please use .toml or .json)", path)
 	}
+}
+
+func Load(path string) (*externalBlueprint.Blueprint, error) {
+	externalBp, err := load(path)
+	if err != nil {
+		return nil, err
+	}
+	// XXX: make convert a method on "Blueprint"
+	// XXX2: make Convert() take a pointer
+	imagesBp := externalBlueprint.Convert(*externalBp)
+	return &imagesBp, nil
 }