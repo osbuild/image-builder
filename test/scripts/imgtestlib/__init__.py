96e3a3c831f68030c9471b0d6f88f5af852abd6b
Achilleas Koutsou <achilleas@koutsou.net>
2026-07-08 16:36:35 +0200

    imgtestlib: don't use star imports in __init__.py
    Make every submodule a relative import and update all external imports.

--- test/scripts/imgtestlib/__init__.py
+++ test/scripts/imgtestlib/__init__.py
@@ -1,8 +1,1 @@
-from .boot import *
-from .build import *
-from .cache import *
-from .core import *
-from .gitlab import *
-from .run import *
-from .testenv import *
-from .vm import *
+from . import boot, build, cache, core, gitlab, run, testenv, vm
