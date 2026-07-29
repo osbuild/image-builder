5f604a56175dde1c14b127727dab581a05e55bfb
Achilleas Koutsou <achilleas@koutsou.net>
2026-07-29 19:50:39 +0200

    image-builder: remove manifestOptions struct
    The function it was used for doesn't exist anymore.
    This reduces the length of generateManifest() considerably as well.

--- cmd/image-builder/manifest.go
+++ cmd/image-builder/manifest.go
@@ -4,34 +4,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-
-	"github.com/osbuild/image-builder/pkg/customizations/subscription"
-	"github.com/osbuild/image-builder/pkg/manifestgen"
-	"github.com/osbuild/image-builder/pkg/osbuild"
-	"github.com/osbuild/image-builder/pkg/ostree"
 )
-
-type manifestOptions struct {
-	ManifestgenOptions manifestgen.Options
-
-	OutputDir                  string
-	OutputFilename             string
-	BlueprintPath              string
-	Ostree                     *ostree.ImageOptions
-	BootcRef                   string
-	BootcInstallerPayloadRef   string
-	BootcOmitDefaultKernelArgs bool
-	BootcRemote                bool
-	ImageSize                  uint64
-	Subscription               *subscription.ImageOptions
-	RpmDownloader              osbuild.RpmDownloader
-	WithSBOM                   bool
-	WithRPMList                bool
-	IgnoreWarnings             bool
-	Preview                    *bool
-
-	ForceRepos []string
-}
 
 func fileWriter(outputDir, filename string, content io.Reader) error {
 	p := filepath.Join(outputDir, filename)
