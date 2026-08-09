2a98e881aa3b1a57c646960f43a5b8e8f37c1141
Simon de Vlieger <cmdr@supakeen.com>
2026-08-09 12:19:14 +0200

    pkg/(distro|image): wire up sysexts
    Generate pipelines in the image generators if sysexts are defined in the
    definitions.
    
    That includes copying around some fields and bits and bobs and injecting
    the appropriate pipelines including one dependent on the format of the
    sysext.
    
    Signed-off-by: Simon de Vlieger <cmdr@supakeen.com>

--- pkg/distro/defs/imagetype.go
+++ pkg/distro/defs/imagetype.go
@@ -77,6 +77,8 @@ type imageType struct {
 	diskImageVPCForceSize *bool
 	diskImageGiBAligned   bool
 
+	sysexts []SysextDef
+
 	supportedPartitioningModes []partition.PartitioningMode
 
 	blueprint blueprintOptions
@@ -196,6 +198,7 @@ func newImageTypeFrom(d *distribution, ar *architecture, imgYAML ImageTypeYAML) 
 	}
 
 	it.partitionTable = basePT
+	it.sysexts = imgYAML.Sysexts(d.ID(), ar.Name())
 
 	it.packageSets = imgYAML.PackageSets(d.ID(), ar.Name())
 
