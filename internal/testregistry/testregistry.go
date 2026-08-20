418349d162a42c4a7353658ec0830cf90fa40433
Achilleas Koutsou <achilleas@koutsou.net>
2026-08-20 13:56:54 +0200

    testregistry: return image IDs alongside digests
    Return the image IDs that were created by the PopulateWithManifestList()
    function alongside the digests for the new images.

--- internal/testregistry/testregistry.go
+++ internal/testregistry/testregistry.go
@@ -449,11 +449,26 @@ func (r *Registry) Host() string {
 	return r.server.Listener.Addr().String()
 }
 
+// Image is a simple struct that holds the image ID and digest of a single
+// image.
+type Image struct {
+	imageID string
+	digest  string
+}
+
+func (img Image) ImageID() string {
+	return img.imageID
+}
+
+func (img Image) Digest() string {
+	return img.digest
+}
+
 // PopulateWithManifestList adds a manifest list (index) to the registry with
 // the given ref. The list contains four containers, one for each architecture
 // (amd64, arm64, s390x, ppc64le).
 // Returns the manifest list digest and a map of all the digests keyed by the architecture.
-func (r *Registry) PopulateWithManifestList(containerRef string) (string, map[string]string, error) {
+func (r *Registry) PopulateWithManifestList(containerRef string) (string, map[string]Image, error) {
 	imageRef := r.Host() + "/" + containerRef
 
 	// Generate four random images, one for each architecture
@@ -461,20 +476,36 @@ func (r *Registry) PopulateWithManifestList(containerRef string) (string, map[st
 	if err != nil {
 		return "", nil, fmt.Errorf("failed to generate random container image: %w", err)
 	}
+	amd64ImageID, err := amd64.ConfigName()
+	if err != nil {
+		return "", nil, err
+	}
 
 	arm64, err := random.Image(1024, 1)
 	if err != nil {
 		return "", nil, fmt.Errorf("failed to generate random container image: %w", err)
+	}
+	arm64ImageID, err := arm64.ConfigName()
+	if err != nil {
+		return "", nil, err
 	}
 
 	s390x, err := random.Image(2014, 1)
 	if err != nil {
 		return "", nil, fmt.Errorf("failed to generate random container image: %w", err)
 	}
+	s390xImageID, err := s390x.ConfigName()
+	if err != nil {
+		return "", nil, err
+	}
 
 	ppc64le, err := random.Image(2014, 1)
 	if err != nil {
 		return "", nil, fmt.Errorf("failed to generate random container image: %w", err)
+	}
+	ppc64leImageID, err := ppc64le.ConfigName()
+	if err != nil {
+		return "", nil, err
 	}
 
 	// Add images to an index and define the architecture
@@ -531,7 +562,6 @@ func (r *Registry) PopulateWithManifestList(containerRef string) (string, map[st
 		return "", nil, fmt.Errorf("failed to push image: %w", err)
 	}
 
-	digests := make(map[string]string, 4)
 	indexDigest, err := index.Digest()
 	if err != nil {
 		return "", nil, err
@@ -540,9 +570,26 @@ func (r *Registry) PopulateWithManifestList(containerRef string) (string, map[st
 	if err != nil {
 		return "", nil, err
 	}
+
+	images := map[string]Image{
+		"amd64": {
+			imageID: amd64ImageID.String(),
+		},
+		"arm64": {
+			imageID: arm64ImageID.String(),
+		},
+		"s390x": {
+			imageID: s390xImageID.String(),
+		},
+		"ppc64le": {
+			imageID: ppc64leImageID.String(),
+		},
+	}
 	for _, imageManifest := range indexManifest.Manifests {
-		digests[imageManifest.Platform.Architecture] = imageManifest.Digest.String()
+		image := images[imageManifest.Platform.Architecture]
+		image.digest = imageManifest.Digest.String()
+		images[imageManifest.Platform.Architecture] = image
 	}
 
-	return indexDigest.String(), digests, nil
+	return indexDigest.String(), images, nil
 }
