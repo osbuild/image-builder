a4c79a44caccdf6145fc4115aedbaf22f5e93336
Achilleas Koutsou <achilleas@koutsou.net>
2026-09-09 18:59:06 +0200

    distro/defs: convert ImageTypeYAML to property
    Instead of embedding ImageTypeYAML in imageType, add it as a regular
    property.  This makes it clear and explicit what gets accessed from the
    YAML backend and what's a property or method of the imageType itself.
    
    The goal is to remove all YAML-backed data and types from the imageType
    and resolve everything as soon as it's loaded, making the imageType as
    static as possible.

--- pkg/distro/defs/options.go
+++ pkg/distro/defs/options.go
@@ -13,7 +13,7 @@ import (
 )
 
 func checkOptionsCommon(t *imageType, bp *blueprint.Blueprint, options distro.ImageOptions) ([]string, error) {
-	if !t.IsOSTreeBasedImageType() && options.OSTree != nil {
+	if !t.ImageTypeYAML.IsOSTreeBasedImageType() && options.OSTree != nil {
 		return nil, fmt.Errorf("OSTree is not supported for %q", t.Name())
 	}
 
@@ -40,7 +40,7 @@ func checkOptionsCommon(t *imageType, bp *blueprint.Blueprint, options distro.Im
 		}
 	}
 
-	if (t.BootISO || t.Bootable) && t.IsOSTreeBasedImageType() {
+	if (t.ImageTypeYAML.BootISO || t.ImageTypeYAML.Bootable) && t.ImageTypeYAML.IsOSTreeBasedImageType() {
 		// ostree-based ISOs require a URL from which to pull a payload commit, this can either be a default URL or one
 		// supplied through options
 		if t.OSTreeURL() == "" && (options.OSTree == nil || options.OSTree.URL == "") {
@@ -123,7 +123,7 @@ func checkOptionsCommon(t *imageType, bp *blueprint.Blueprint, options distro.Im
 	dcp := policies.CustomDirectoriesPolicies
 	fcp := policies.CustomFilesPolicies
 
-	if t.IsOSTreeBasedImageType() {
+	if t.ImageTypeYAML.IsOSTreeBasedImageType() {
 		dcp = policies.OstreeCustomDirectoriesPolicies
 		fcp = policies.OstreeCustomFilesPolicies
 	}
